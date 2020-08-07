6eefb16f8520c4b868d001c13f1a858fb7900619
7596ff <7596ff@users.noreply.github.com>
2020-08-07 17:39:17 +0000

    deploy: c29f07bc55ae489bc352b25a229617fdae39f928

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,7 +1,7 @@
 (function() {var implementors = {};
-implementors["twilight_builders"] = [{"text":"impl Default for EmbedBuilder","synthetic":false,"types":[]}];
 implementors["twilight_cache_inmemory"] = [{"text":"impl Default for InMemoryConfig","synthetic":false,"types":[]},{"text":"impl Default for InMemoryConfigBuilder","synthetic":false,"types":[]},{"text":"impl Default for InMemoryCache","synthetic":false,"types":[]}];
 implementors["twilight_command_parser"] = [{"text":"impl&lt;'a&gt; Default for CommandParserConfig&lt;'a&gt;","synthetic":false,"types":[]}];
+implementors["twilight_embed_builder"] = [{"text":"impl Default for EmbedAuthorBuilder","synthetic":false,"types":[]},{"text":"impl Default for EmbedBuilder","synthetic":false,"types":[]}];
 implementors["twilight_gateway"] = [{"text":"impl Default for ShardScheme","synthetic":false,"types":[]},{"text":"impl Default for LocalQueue","synthetic":false,"types":[]},{"text":"impl Default for Stage","synthetic":false,"types":[]}];
 implementors["twilight_http"] = [{"text":"impl Default for ClientBuilder","synthetic":false,"types":[]},{"text":"impl Default for Ratelimiter","synthetic":false,"types":[]},{"text":"impl Default for AllowedMentions","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; Default for AllowedMentionsBuilder&lt;'a, Unspecified, Unspecified, Unspecified&gt;","synthetic":false,"types":[]}];
 implementors["twilight_lavalink"] = [{"text":"impl Default for Resume","synthetic":false,"types":[]},{"text":"impl Default for PlayerManager","synthetic":false,"types":[]}];
