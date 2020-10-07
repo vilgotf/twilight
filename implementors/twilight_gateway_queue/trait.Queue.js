e13494fe611cd1aa13ce4f639b8151ef5402fc4c
Erk- <Erk-@users.noreply.github.com>
2020-10-07 10:30:40 +0000

    deploy: 06fa66917f9dc7348db35ac53caa2c58884348af

--- implementors/twilight_gateway_queue/trait.Queue.js
+++ implementors/twilight_gateway_queue/trait.Queue.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["twilight_gateway"] = [];
+implementors["twilight_gateway_queue"] = [];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
