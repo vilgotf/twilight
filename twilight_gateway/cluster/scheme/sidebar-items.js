63b322a28ea40fdb1f973b025bdbe74d89fd1242
7596ff <7596ff@users.noreply.github.com>
2021-03-29 18:25:25 +0000

    deploy: f241c47f2ef1f241256e98d447702d9d27213bee

---
+++ twilight_gateway/cluster/scheme/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["ShardScheme","The method of sharding to use."],["ShardSchemeRangeError","Starting a cluster failed."]],"struct":[["ShardSchemeIter","Iterator of shard IDs based on a shard scheme."]]});
